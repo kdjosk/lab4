4195467fffba7578fe3e926c91d3a67e7d8416e6
Joskowiak <krzysztof.josk@gmail.com>
2021-05-25 12:18:33 +0200

    remove ItemPointer from frostunittests

--- tests/FrostfruitUnitTests.cc
+++ tests/FrostfruitUnitTests.cc
@@ -27,7 +27,7 @@ TEST(FrostfruitTest, addItem) {
 
   Frostfruit app = Frostfruit();
   std::stringstream output;
-  app.addItem(ItemPointer{new NormalItem("+7 Yellow Vest", 10, 20)});
+  app.addItem(new NormalItem("+7 Yellow Vest", 10, 20));
   app.printItems(output);
 
   EXPECT_EQ(output.str(), "+7 Yellow Vest, 10, 20");
@@ -37,12 +37,12 @@ TEST(FrostfruitTest, updateQuality) {
 
   Frostfruit app = Frostfruit();
   std::stringstream output;
-  ItemPointer item = ItemPointer{new MockItem()};
+  MockItem item;
 
-  EXPECT_CALL(*item, update()).Times(1);
-  EXPECT_CALL(*item, getName()).WillOnce(Return("+7 Yellow Vest"));
-  EXPECT_CALL(*item, getDaysRemaining()).WillOnce(Return(2));
-  EXPECT_CALL(*item, getQuality()).Times(1).WillOnce(Return(2));
+  EXPECT_CALL(item, update()).Times(1);
+  EXPECT_CALL(item, getName()).WillOnce(Return("+7 Yellow Vest"));
+  EXPECT_CALL(item, getDaysRemaining()).WillOnce(Return(2));
+  EXPECT_CALL(item, getQuality()).Times(1).WillOnce(Return(2));
 
   app.addItem(item);
   app.updateQuality();
