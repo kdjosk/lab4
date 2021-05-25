7817349ce17f0f998ddc1baa187b9803ba8dce79
Krzysztof Jóskowiak <krzysztof.josk@gmail.com>
2021-05-25 11:46:34 +0200

    delete commented code

--- src/Frostfruit.cc
+++ src/Frostfruit.cc
@@ -16,55 +16,7 @@ void Frostfruit::updateQuality() {
     items_[i]->update();
   }
 }
-/*
-void Frostfruit::updateQuality() {
-  for (int i = 0; i < items_.size(); i++) {
-    if (items_[i].t != item1 && items_[i].t != item2) {
-      if (items_[i].v > 0) {
-        if (items_[i].t != item3) {
-          items_[i].v -= 1;
-        }
-      }
-    } else {
-      if (items_[i].v < 50) {
-        items_[i].v = items_[i].v + 1;
-        if (items_[i].t == item2) {
-          if (items_[i].d < 11) {
-            if (items_[i].v < 50) {
-              items_[i].v += 1;
-            }
-          }
-          if (items_[i].d < 6) {
-            if (items_[i].v < 50) {
-              items_[i].v++;
-            }
-          }
-        }
-      }
-    }
-    if (items_[i].t != item3) {
-      --items_[i].d;
-    }
-    if (items_[i].d < 0) {
-      if (items_[i].t != item1) {
-        if (items_[i].t != item2) {
-          if (items_[i].v > 0) {
-            if (items_[i].t != item3) {
-              items_[i].v = items_[i].v - 1;
-            }
-          }
-        } else {
-          items_[i].v = items_[i].v - items_[i].v;
-        }
-      } else {
-        if (items_[i].v < 50) {
-          ++items_[i].v;
-        }
-      }
-    }
-  }
-}
-*/
+
 void Frostfruit::addItem(IItem* item) { items_.push_back(item); }
 /*
 void Frostfruit::printItems() {
